3e1618d4f6d5d01bead4420f84781b362007d58f
venuchilukala <venuchilukala07@gmail.com>
2025-02-18 17:38:33 +0530

    initialized app using firebaseConfig

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCwLk64XmwS44mudqf-4Sl59LeRd78lbNY",
+  authDomain: "fire-base-react-app-vc203.firebaseapp.com",
+  projectId: "fire-base-react-app-vc203",
+  storageBucket: "fire-base-react-app-vc203.firebasestorage.app",
+  messagingSenderId: "26240604548",
+  appId: "1:26240604548:web:5c5341c54913f079a465d1",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
